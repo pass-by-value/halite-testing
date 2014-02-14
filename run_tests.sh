7800e8af92f8db63ce519f2e2523b52d6aae32c4
Aditya Kulkarni <adi@saltstack.com>
2014-02-14 15:57:38 -0500

    Change path

--- run_tests.sh
+++ run_tests.sh
@@ -7,5 +7,5 @@ salt halite_ubuntu_master cmd.run 'salt \* state.highstate'
 salt halite_ubuntu_master state.sls run-halite-testing
 salt halite_ubuntu_master cp.push /root/halite_test_results.xml
 salt halite_ubuntu_master state.sls delete-minion
-mv /var/cache/salt/master/minions/halite_ubuntu_master/files/tmp/halite_test_results.xml $HOME/jenkins
+mv /var/cache/salt/master/minions/halite_ubuntu_master/files/root/halite_test_results.xml $HOME/jenkins
 salt-cloud -y -d rackspace_512 halite_ubuntu_master
