f906d5110ba206e2c58d9b38b281993655db8c1e
Aditya Kulkarni <adi@saltstack.com>
2014-02-14 16:14:37 -0500

    Changes to runner script

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+set -x
 salt-cloud -y -p rackspace_512 halite_ubuntu_master
 salt halite_ubuntu_master state.highstate
 salt halite_ubuntu_master state.sls create-minion
@@ -7,5 +7,5 @@ salt halite_ubuntu_master cmd.run 'salt \* state.highstate'
 salt halite_ubuntu_master state.sls run-halite-testing
 salt halite_ubuntu_master cp.push /root/halite_test_results.xml
 salt halite_ubuntu_master state.sls delete-minion
-mv /var/cache/salt/master/minions/halite_ubuntu_master/files/root/halite_test_results.xml $HOME/jenkins
+mv /var/cache/salt/master/minions/halite_ubuntu_master/files/root/halite_test_results.xml $HOME/
 salt-cloud -y -d rackspace_512 halite_ubuntu_master
